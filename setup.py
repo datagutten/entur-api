7464a2de9e7ed526e3dea11e6a27596cfe4dae28
datagutten <datagutten@datagutten.net>
2019-07-18 10:42:19 +0200

    Use correct license string

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPL',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
