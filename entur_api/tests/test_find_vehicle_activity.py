8a06e6b52ae43a9c42374889521097c91b94ac33
datagutten <datagutten@datagutten.net>
2019-07-21 20:59:29 +0200

    Fix file paths in tests

--- entur_api/tests/test_find_vehicle_activity.py
+++ entur_api/tests/test_find_vehicle_activity.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from entur_api.siri import Siri
@@ -5,9 +6,11 @@ from entur_api.siri import Siri
 
 class TestFindVehicleActivity(TestCase):
     siri = None
+    file_path = None
 
     def setUp(self):
-        self.siri = Siri('datagutten-tests', file='test_data/vm.xml')
+        self.file_path = os.path.join(os.path.dirname(__file__), 'test_data')
+        self.siri = Siri('datagutten-tests', file=os.path.join(self.file_path, 'vm.xml'))
         pass
 
     def test_find(self):
@@ -24,7 +27,7 @@ class TestFindVehicleActivity(TestCase):
         self.assertEqual('101130', act.vehicle())
 
     def test_find_with_line_and_quay(self):
-        self.siri = Siri('datagutten-tests', file='test_data/vm_schedule.xml')
+        self.siri = Siri('datagutten-tests', file=os.path.join(self.file_path, 'vm_schedule.xml'))
         act = self.siri.find_vehicle_activity(
             origin_aimed_departure_time='2019-07-21T16:40:00+02:00',
             line='RUT:Line:83',
--- entur_api/tests/test_siri.py
+++ entur_api/tests/test_siri.py
@@ -1,9 +1,15 @@
+import os
 from unittest import TestCase
 
 from entur_api.siri import Siri
 
 
 class SiriTest(TestCase):
+    file_path = None
+
+    def setUp(self):
+        self.file_path = os.path.join(os.path.dirname(__file__), 'test_data')
+
     def test_vehicle_activities(self):
         siri = Siri('datagutten-entur-api-test', line='RUT:Line:83')
         activities = siri.vehicle_activities()
@@ -14,7 +20,7 @@ class SiriTest(TestCase):
             self.assertEqual('83', activity.line_name())
 
     def test_location(self):
-        siri = Siri('datagutten-entur-api-test', file='test_data/vm.xml')
+        siri = Siri('datagutten-entur-api-test', file=os.path.join(self.file_path, 'vm.xml'))
         activities = siri.vehicle_activities()
         for act in activities:
             self.assertIsNotNone(act.previous_call()['StopPointRef'])
