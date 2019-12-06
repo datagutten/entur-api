abdf151df030681dc9ef84bb9b84a6f1f516936b
datagutten <datagutten@datagutten.net>
2019-12-06 17:51:11 +0100

    Cache requests

--- entur_api/enturcommon.py
+++ entur_api/enturcommon.py
@@ -1,7 +1,12 @@
+from time import time
+
 import requests
 
 
 class EnturCommon:
+    cache = {}
+    last_request = {}
+
     def __init__(self, client):
         self.client = client
 
@@ -14,8 +19,17 @@ class EnturCommon:
             else:
                 return request.text
         else:
-            raise Exception('Query failed to run by returning code of %s' %
-                            request.status_code)
+            raise Exception('Query failed to run by returning code of %s with URL %s' %
+                            (request.status_code, url))
+
+    def cached_get(self, url, json=True):
+        if url in self.cache and time()-self.last_request[url] < 30:
+            return self.cache[url]
+        else:
+            response = self.get(url, json)
+            self.cache[url] = response
+            self.last_request[url] = time()
+            return response
 
     def rest_query(self, data_type='vm', operator='RUT', line_ref=None):
         url = 'https://api.entur.io/realtime/v1/rest/%s?' % data_type
@@ -23,5 +37,4 @@ class EnturCommon:
             url += 'datasetId=%s&' % operator
         if line_ref:
             url += 'LineRef=%s&' % line_ref
-        # print(url)
-        return self.get(url, json=False)
+        return self.cached_get(url, json=False)
