d95e374baabdebaa18f7d6f01673bd28cc4d2f17
datagutten <datagutten@datagutten.net>
2020-01-18 12:46:02 +0100

    Add origin parameter to get specific departure

--- entur_api/tests/test_find_vehicle_activity.py
+++ entur_api/tests/test_find_vehicle_activity.py
@@ -15,13 +15,15 @@ class TestFindVehicleActivity(TestCase):
 
     def test_find(self):
         act = self.siri.find_vehicle_activity(
-            origin_aimed_departure_time='2019-07-21T14:10:00+02:00')
+            origin_aimed_departure_time='2019-07-21T14:10:00+02:00',
+            origin_quay='NSR:Quay:6263')
         self.assertEqual('Unibuss:8301:00810', act.block_ref())
         self.assertEqual('101130', act.vehicle())
 
     def test_find_with_line(self):
         act = self.siri.find_vehicle_activity(
             origin_aimed_departure_time='2019-07-21T14:10:00+02:00',
+            origin_quay='NSR:Quay:6263',
             line='RUT:Line:83')
         self.assertEqual('Unibuss:8301:00810', act.block_ref())
         self.assertEqual('101130', act.vehicle())
